33e468285885ce04caa1768caa6cb1463df672df
Vedansh Saini <vedanshsaini06@gmail.com>
2025-11-16 04:52:48 +0000

    feat: implement store management API and integrate image upload functionality

---
+++ app/api/store/stock-toggle/route.js
@@ -0,0 +1,48 @@
+import prisma from "@/lib/prisma";
+import authSeller from "@/middlewares/authSeller";
+import { getAuth } from "@clerk/nextjs/server";
+
+// toggle stock of a product
+export async function POST(request) {
+  try {
+    const { userId } = getAuth(request);
+    const { productId } = await request.json();
+
+    if (!productId) {
+      return NextResponse.json(
+        { error: "missing details: producyId" },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const storeId = await authSeller(userId);
+
+    if (!storeId) {
+      return NextResponse.json({ error: "not authorized" }, { status: 401 });
+    }
+
+    // check if product exists
+    const product = await prisma.product.findFirst({
+      where: { productId, storeId },
+    });
+
+    if (!product) {
+      return NextResponse.json({ error: "no product found" }, { status: 404 });
+    }
+
+    await prisma.product.update({
+      where: { id: productId },
+      data: { inStock: !product.inStock },
+    });
+
+    return NextResponse.json({ message: "Product stock updated successfully" });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: error.code || error.message },
+      { status: 400 }
+    );
+  }
+}
