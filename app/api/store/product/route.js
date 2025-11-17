e66521d309e982e8ff4c0785761a048a5c51c700
Vedansh Saini <vedanshsaini06@gmail.com>
2025-11-17 15:12:31 +0000

    enhance store creation and management features with admin approval and dashboard integration

--- app/api/store/product/route.js
+++ app/api/store/product/route.js
@@ -2,7 +2,7 @@ import imagekit from "@/configs/imageKit";
 import prisma from "@/lib/prisma";
 import authSeller from "@/middlewares/authSeller";
 import { getAuth } from "@clerk/nextjs/server";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 // Add a new product
 export async function POST(request) {
--- app/api/store/stock-toggle/route.js
+++ app/api/store/stock-toggle/route.js
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma";
 import authSeller from "@/middlewares/authSeller";
 import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 // toggle stock of a product
 export async function POST(request) {
