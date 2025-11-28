faa3f4a9a93ab2a59f04077fbed3ace018ee5a24
Vedansh Saini <vedanshsaini06@gmail.com>
2025-11-28 16:47:56 +0000

    add product fetching logic and update initial state in product slice

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -1,16 +1,23 @@
-'use client'
+"use client";
 import Banner from "@/components/Banner";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchProducts } from "@/lib/features/product/productSlice";
 
 export default function PublicLayout({ children }) {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchProducts({}));
+  }, []);
 
-    return (
-        <>
-            <Banner />
-            <Navbar />
-            {children}
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Banner />
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
 }
