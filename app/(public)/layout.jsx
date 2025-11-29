425f519ae604a80eb19409b0c93a964ed21a2ee9
Vedansh Saini <vedanshsaini06@gmail.com>
2025-11-29 15:15:02 +0000

    address management with API integration and enhance cart functionality

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -3,14 +3,36 @@ import Banner from "@/components/Banner";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "@/lib/features/product/productSlice";
+import { useUser, useAuth } from "@clerk/nextjs";
+import { fetchCart, uploadCart } from "@/lib/features/cart/cartSlice";
+import { fetchAddress } from "@/lib/features/address/addressSlice";
 
 export default function PublicLayout({ children }) {
   const dispatch = useDispatch();
+
+  const { user } = useUser();
+  const { getToken } = useAuth();
+
+  const { cartItems } = useSelector((state) => state.cart);
+
   useEffect(() => {
     dispatch(fetchProducts({}));
   }, []);
+
+  useEffect(() => {
+    if (user) {
+      dispatch(fetchCart({ getToken }));
+      dispatch(fetchAddress({ getToken }));
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (user) {
+      dispatch(uploadCart({ getToken }));
+    }
+  }, [cartItems]);
 
   return (
     <>
